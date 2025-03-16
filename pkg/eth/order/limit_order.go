8d4a0181e67733b8ec5388ceea2ec4a91003119d
Tinkler <tinkler@163.com>
2025-03-16 17:08:29 +0800

    feat: not struct

--- pkg/eth/order/limit_order.go
+++ pkg/eth/order/limit_order.go
@@ -10,32 +10,64 @@ import (
 var LimitOrderExecuteInputABI abi.Arguments
 
 func init() {
-	limitOrderExecuteInputType, err := abi.NewType("tuple", "", []abi.ArgumentMarshaling{
-		{Name: "order", Type: "tuple", Components: []abi.ArgumentMarshaling{
+	addressInputType, err := abi.NewType("address", "", nil)
+	if err != nil {
+		panic(err)
+	}
+	uint256InputType, err := abi.NewType("uint256", "", nil)
+	if err != nil {
+		panic(err)
+	}
+	{
+		// order
+		orderInputType, err := abi.NewType("tuple", "", []abi.ArgumentMarshaling{
 			{Name: "account", Type: "address"},
 			{Name: "index", Type: "uint256"},
 			{Name: "orderType", Type: "uint256"},
 			{Name: "executeFee", Type: "uint256"},
-		}},
-		{Name: "routes", Type: "tuple[]", Components: []abi.ArgumentMarshaling{
+		})
+		if err != nil {
+			panic(err)
+		}
+		LimitOrderExecuteInputABI = append(LimitOrderExecuteInputABI, abi.Argument{Type: orderInputType})
+	}
+	{
+		// isExpired
+		boolType, err := abi.NewType("bool", "", nil)
+		if err != nil {
+			panic(err)
+		}
+		LimitOrderExecuteInputABI = append(LimitOrderExecuteInputABI, abi.Argument{Type: boolType})
+	}
+	// tokenIn
+	LimitOrderExecuteInputABI = append(LimitOrderExecuteInputABI, abi.Argument{Type: addressInputType})
+	// tokenOut
+	LimitOrderExecuteInputABI = append(LimitOrderExecuteInputABI, abi.Argument{Type: addressInputType})
+	// remainingAmountIn
+	LimitOrderExecuteInputABI = append(LimitOrderExecuteInputABI, abi.Argument{Type: uint256InputType})
+	{
+		// routes
+		inputType, err := abi.NewType("tuple[]", "", []abi.ArgumentMarshaling{
 			{Name: "dexId", Type: "uint16"},
 			{Name: "tokenIn", Type: "address"},
 			{Name: "tokenOut", Type: "address"},
 			{Name: "amountIn", Type: "uint256"},
 			{Name: "amountOutMin", Type: "uint256"},
 			{Name: "extraData", Type: "bytes"},
-		}},
-		{Name: "tokenIn", Type: "address"},
-		{Name: "tokenOut", Type: "address"},
-		{Name: "amountIn", Type: "uint256"},
-		{Name: "amountOutMin", Type: "uint256"},
-		{Name: "amountOutExpected", Type: "uint256"},
-		{Name: "feeReceiver", Type: "address"},
-	})
-	if err != nil {
-		panic(err)
+		})
+		if err != nil {
+			panic(err)
+		}
+		LimitOrderExecuteInputABI = append(LimitOrderExecuteInputABI, abi.Argument{Type: inputType})
 	}
-	LimitOrderExecuteInputABI = append(LimitOrderExecuteInputABI, abi.Argument{Type: limitOrderExecuteInputType})
+	// amountIn
+	LimitOrderExecuteInputABI = append(LimitOrderExecuteInputABI, abi.Argument{Type: uint256InputType})
+	// amountOutMin
+	LimitOrderExecuteInputABI = append(LimitOrderExecuteInputABI, abi.Argument{Type: uint256InputType})
+	// amountOutExpected
+	LimitOrderExecuteInputABI = append(LimitOrderExecuteInputABI, abi.Argument{Type: uint256InputType})
+	// feeReceiver
+	LimitOrderExecuteInputABI = append(LimitOrderExecuteInputABI, abi.Argument{Type: addressInputType})
 }
 
 type SwapRoute struct {
@@ -51,6 +83,7 @@ type LimitOrderExecuteInput struct {
 	Order             Order
 	TokenIn           common.Address
 	TokenOut          common.Address
+	RemainingAmountIn *big.Int
 	Routes            []SwapRoute
 	AmountIn          *big.Int
 	AmountOutMin      *big.Int
--- pkg/eth/order/limit_order_test.go
+++ pkg/eth/order/limit_order_test.go
@@ -28,71 +28,26 @@ func TestLimitOrderExecuteInput_PackUnpack(t *testing.T) {
 		},
 	}
 
-	input := LimitOrderExecuteInput{
-		Order:             order,
-		TokenIn:           common.HexToAddress("0x4444444444444444444444444444444444444444"),
-		TokenOut:          common.HexToAddress("0x5555555555555555555555555555555555555555"),
-		Routes:            routes,
-		AmountIn:          big.NewInt(10000),
-		AmountOutMin:      big.NewInt(9500),
-		AmountOutExpected: big.NewInt(10000),
-		FeeReceiver:       common.HexToAddress("0x6666666666666666666666666666666666666666"),
-	}
-
 	// Pack the input
-	packed, err := LimitOrderExecuteInputABI.Pack(input)
+	packed, err := LimitOrderExecuteInputABI.Pack(
+		order,
+		false,
+		common.HexToAddress("0x4444444444444444444444444444444444444444"),
+		common.HexToAddress("0x5555555555555555555555555555555555555555"),
+		big.NewInt(1),
+		routes,
+		big.NewInt(10000),
+		big.NewInt(9500),
+		big.NewInt(10000),
+		common.HexToAddress("0x6666666666666666666666666666666666666666"),
+	)
 	assert.NoError(t, err)
 	assert.NotEmpty(t, packed)
 
 	// Unpack the packed data
-	_, err = LimitOrderExecuteInputABI.Unpack(packed)
+	unpacked, err := LimitOrderExecuteInputABI.Unpack(packed)
 	assert.NoError(t, err)
 
-	// Define anonymous struct type to match ABI unpacking with JSON tags
-	// type unpackedStruct struct {
-	// 	Order struct {
-	// 		Account    common.Address `json:"account"`
-	// 		Index      *big.Int       `json:"index"`
-	// 		OrderType  *big.Int       `json:"orderType"`
-	// 		ExecuteFee *big.Int       `json:"executeFee"`
-	// 	} `json:"order"`
-	// 	Routes []struct {
-	// 		DexId        uint16         `json:"dexId"`
-	// 		TokenIn      common.Address `json:"tokenIn"`
-	// 		TokenOut     common.Address `json:"tokenOut"`
-	// 		AmountIn     *big.Int       `json:"amountIn"`
-	// 		AmountOutMin *big.Int       `json:"amountOutMin"`
-	// 		ExtraData    []uint8        `json:"extraData"`
-	// 	} `json:"routes"`
-	// 	TokenIn           common.Address `json:"tokenIn"`
-	// 	TokenOut          common.Address `json:"tokenOut"`
-	// 	AmountIn          *big.Int       `json:"amountIn"`
-	// 	AmountOutMin      *big.Int       `json:"amountOutMin"`
-	// 	AmountOutExpected *big.Int       `json:"amountOutExpected"`
-	// 	FeeReceiver       common.Address `json:"feeReceiver"`
-	// }
+	t.Logf("%v", unpacked)
 
-	// Get the unpacked struct
-	// unpackedInput := unpacked[0].(unpackedStruct)
-
-	// Compare original and unpacked values
-	// assert.Equal(t, input.Order.Account, unpackedInput.Order.Account)
-	// assert.Equal(t, input.Order.Index, unpackedInput.Order.Index)
-	// assert.Equal(t, input.Order.OrderType, unpackedInput.Order.OrderType)
-	// assert.Equal(t, input.Order.ExecuteFee, unpackedInput.Order.ExecuteFee)
-
-	// assert.Equal(t, input.TokenIn, unpackedInput.TokenIn)
-	// assert.Equal(t, input.TokenOut, unpackedInput.TokenOut)
-	// assert.Equal(t, input.AmountIn, unpackedInput.AmountIn)
-	// assert.Equal(t, input.AmountOutMin, unpackedInput.AmountOutMin)
-	// assert.Equal(t, input.AmountOutExpected, unpackedInput.AmountOutExpected)
-	// assert.Equal(t, input.FeeReceiver, unpackedInput.FeeReceiver)
-
-	// assert.Len(t, unpackedInput.Routes, 1)
-	// assert.Equal(t, input.Routes[0].DexId, unpackedInput.Routes[0].DexId)
-	// assert.Equal(t, input.Routes[0].TokenIn, unpackedInput.Routes[0].TokenIn)
-	// assert.Equal(t, input.Routes[0].TokenOut, unpackedInput.Routes[0].TokenOut)
-	// assert.Equal(t, input.Routes[0].AmountIn, unpackedInput.Routes[0].AmountIn)
-	// assert.Equal(t, input.Routes[0].AmountOutMin, unpackedInput.Routes[0].AmountOutMin)
-	// assert.Equal(t, input.Routes[0].ExtraData, unpackedInput.Routes[0].ExtraData)
 }
